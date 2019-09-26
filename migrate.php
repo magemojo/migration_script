fde35562458871f6bab7e1e7732e8d0599151dec
jwisecarver-mm <50086133+jwisecarver-mm@users.noreply.github.com>
2019-09-26 11:04:18 -0700

    fix spacing on deploy_m2 function

--- migrate.php
+++ migrate.php
@@ -362,11 +362,10 @@ function blackhole_m1_tables($options,$remote_db_info) {
 }
 
 function deploy_m2($options) {
-	echo "php ".$options['web_root']."bin/magento maintenance:enable";
-	run_command("php ".$options['web_root']."bin/magento maintenance:enable");
-	run_command("php ".$options['web_root']."bin/magento deploy:mode:set production");
-	run_command("php ".$options['web_root']."bin/magento maintenance:disable");
-	run_command("php ".$options['web_root']."bin/magento cache:clean");
+	run_command("php ".$options['web_root']." bin/magento maintenance:enable");
+	run_command("php ".$options['web_root']." bin/magento deploy:mode:set production");
+	run_command("php ".$options['web_root']." bin/magento maintenance:disable");
+	run_command("php ".$options['web_root']." bin/magento cache:clean");
 }
 
 function reindex_m1($web_root) {
